3e56e8569d91f41180c54caf24dbeea753f782e9
in0ho1no <yin0ho1no@gmail.com>
2024-06-09 03:41:31 +0000

    単純に検索結果をコピーできる状態

--- capture-search-result/src/extension.ts
+++ capture-search-result/src/extension.ts
@@ -3,6 +3,21 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "capture-search-result" is now active!');
+
+	const copySearchResultsCommand = vscode.commands.registerCommand('capture-search-result.copySearchResults', async () => {
+		// 表示中のエディタを取得する
+		const searchEditor = vscode.window.activeTextEditor;
+		if (!searchEditor) {
+			vscode.window.showWarningMessage('No active search results to copy');
+			return;
+		}
+
+		// 表示中のテキストをコピーする
+		const searchResults = searchEditor.document.getText();
+		vscode.env.clipboard.writeText(searchResults);
+	});
+	
+	context.subscriptions.push(copySearchResultsCommand);
 }
 
 // This method is called when your extension is deactivated
